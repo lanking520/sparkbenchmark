b30d74ba0b8bb87804abbb04373ab3fa310a6e1f
Qing Lan <qingla@amazon.com>
2020-07-20 10:58:55 -0700

    fix bug on python script

--- python/sparktest.py
+++ python/sparktest.py
@@ -9,7 +9,7 @@ PARTITION_NUM = 10
 def downloadFromUrl(url):
     tmp = tempfile.gettempdir()
     modelDir = tmp + "/benchmodel"
-    modelName = url.split("/")[-1].split("\\.")[0]
+    modelName = url.split("/")[-1].split(".")[0]
     modelPath = modelDir + "/" + modelName + ".pt"
     zipPath = modelDir + "/" + modelName + ".zip"
     if os.path.exists(modelPath):
@@ -29,7 +29,6 @@ def inferenceOnPartion(iter):
     file = downloadFromUrl("https://djl-ai.s3.amazonaws.com/resources/demo/pytorch/traced_resnet18.zip")
     model = torch.jit.load(file)
     model.eval()
-    model.share_memory()
     result = []
     for _ in iter:
         t = torch.ones((1, 3, 224, 224))
