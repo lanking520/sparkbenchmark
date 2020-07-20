18c363ac4459e59812130ef48f010e1ef771b203
Qing Lan <qingla@amazon.com>
2020-07-20 16:44:56 -0700

    fix

--- standalone/startspark.sh
+++ standalone/startspark.sh
@@ -4,5 +4,5 @@ export SPARK_MASTER_HOST=localhost
 export SPARK_WORKER_INSTANCES=1
 export PYSPARK_DRIVER_PYTHON=python3
 export PYSPARK_PYTHON=python3
-./spark/sbin/start-master.sh
-./spark/sbin/start-slave.sh spark://localhost:7077
+../spark/sbin/start-master.sh
+../spark/sbin/start-slave.sh spark://localhost:7077
