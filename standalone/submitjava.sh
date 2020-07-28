71576880776869e10ff90a90693ad1be4c4e794c
Qing Lan <qingla@amazon.com>
2020-07-28 16:00:02 -0700

    fix

--- standalone/submitjava.sh
+++ standalone/submitjava.sh
@@ -3,4 +3,4 @@
 cd java/
 ./gradlew clean shadowJar
 cd ..
-time ../spark/bin/spark-submit --files players.csv --jars java/build/libs/java-1.0-SNAPSHOT-all.jar
+time ../spark/bin/spark-submit --files players.csv java/build/libs/java-1.0-SNAPSHOT-all.jar
